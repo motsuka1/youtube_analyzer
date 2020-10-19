3cd6b6028649f3312b38ee165e0482ba9ebe30ce
Masaki Otsuka <motsuka@soka.edu>
2020-10-19 15:43:35 +0900

    show data on home.html

--- statsgetter/views.py
+++ statsgetter/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .youtube_bundle import YoutubeBundle
 from .models import *
+import datetime
 
 # Create your views here.
 def home(request):
-    context = {}
+    channel_title = "python enginner"
+    yt = YoutubeBundle(channel_title)
+    results = yt.get_stats_bundle()
+    for result in results:
+        registered_date = datetime.datetime.strptime(result["snippet"]["publishedAt"], "%Y-%m-%dT%H:%M:%SZ")
+        registered_date = registered_date.strftime("%Y/%m/%d")
+        channel_stats = {
+            'title' : result["snippet"]["title"],
+            'id' : result["id"],
+            'registered_date' : registered_date,
+            'subscriberCount' : result["statistics"]["subscriberCount"],
+            'viewCount' : result["statistics"]["viewCount"],
+            'videoCount' : result["statistics"]["videoCount"],
+        }
+    context = {'channel_stats': channel_stats}
     return render(request, 'statsgetter/home.html', context)
--- statsgetter/youtube_scraper.py
+++ statsgetter/youtube_scraper.py
@@ -6,11 +6,11 @@ class YTscraper:
     def __init__(self, channel_title):
         self.channel_title = channel_title
 
-    def get_channel_id_from_title(self):
+    def get_channel_id_or_username_from_title(self):
         chrome_options = webdriver.ChromeOptions()
         '''will use in heroku below'''
         # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         # chrome_options.add_argument("--disable-dev-shm-usage")
         # chrome_options.add_argument("--no-sandbox")
         # chrome_options.add_argument(' -- incognito')
@@ -30,7 +30,9 @@ class YTscraper:
         channel_url = driver.find_element_by_css_selector('#contents > ytd-channel-renderer').find_element_by_xpath(".//div/div/a").get_attribute("href")
         if "youtube.com/channel/" in channel_url:
             channel_id = channel_url.split("youtube.com/channel/", 1)[1]
+            driver.quit()
             return [True, channel_id]
         else:
             channel_username = channel_url.split("youtube.com/user/", 1)[1]
+            driver.quit()
             return [False, channel_username]
