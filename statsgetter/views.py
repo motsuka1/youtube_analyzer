4ea59db7088a575bfd717ab788d17d6155518905
Masaki Otsuka <motsuka@soka.edu>
2020-10-19 14:19:25 +0900

    add view and template

--- statsgetter/views.py
+++ statsgetter/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home(request):
+    context = {}
+    return render(request, 'statsgetter/home.html', context)
