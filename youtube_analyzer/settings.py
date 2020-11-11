d78ca9b731bb8cfc8fa0701305d36a975092f4f6
Masaki Otsuka <motsuka@soka.edu>
2020-11-11 10:07:30 +0900

    fix front

--- youtube_analyzer/settings.py
+++ youtube_analyzer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.33']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
